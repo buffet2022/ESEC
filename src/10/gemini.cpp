3a132229c5d01bb9f805b75e596cd4fc0372e14c
Buffet <211870213@smail.nju.edu.cn>
2024-04-07 21:37:24 +0800

    更新内容

--- src/10/gemini.cpp
+++ src/10/gemini.cpp
@@ -2,22 +2,70 @@ class Solution {
 public:
     int minSwapsCouples(vector<int>& row) {
         int n = row.size() / 2;
-        vector<int> pos(n * 2); // Store the position of each person
-        for (int i = 0; i < row.size(); ++i) {
-            pos[row[i]] = i;
+        // 并查集
+        UnionFind uf(n);
+
+        for (int i = 0; i < n; i++) {
+            // 将情侣的根节点合并
+            uf.unionSet(row[i] / 2, row[i + n] / 2);
         }
 
-        int swaps = 0;
-        for (int i = 0; i < row.size(); i += 2) {
-            int partner = row[i] ^ 1; // Get the partner of the current person
-            if (row[i + 1] != partner) {
-                // Swap the partner with the next person
-                swap(row[i + 1], row[pos[partner]]);
-                swap(pos[row[i + 1]], pos[partner]);
-                ++swaps;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            // 如果当前位置不是情侣的位置
+            if (row[i] != row[i + n]) {
+                // 找到情侣的根节点
+                int root1 = uf.findSet(row[i] / 2);
+                int root2 = uf.findSet(row[i + n] / 2);
+                // 如果两个根节点不同，则需要交换
+                if (root1 != root2) {
+                    ans++;
+                }
             }
         }
 
-        return swaps;
+        return ans;
     }
+
+private:
+    class UnionFind {
+    public:
+        UnionFind(int n) {
+            parents = vector<int>(n);
+            ranks = vector<int>(n);
+            for (int i = 0; i < n; i++) {
+                parents[i] = i;
+                ranks[i] = 0;
+            }
+        }
+
+        void unionSet(int x, int y) {
+            int rootX = findSet(x);
+            int rootY = findSet(y);
+            if (rootX == rootY) {
+                return;
+            }
+
+            if (ranks[rootX] < ranks[rootY]) {
+                parents[rootX] = rootY;
+            } else if (ranks[rootX] > ranks[rootY]) {
+                parents[rootY] = rootX;
+            } else {
+                parents[rootY] = rootX;
+                ranks[rootX]++;
+            }
+        }
+
+        int findSet(int x) {
+            if (parents[x] != x) {
+                parents[x] = findSet(parents[x]);
+            }
+
+            return parents[x];
+        }
+
+    private:
+        vector<int> parents;
+        vector<int> ranks;
+    };
 };
